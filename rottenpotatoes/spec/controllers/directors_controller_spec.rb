d2165d0d3bd06df4e8bd69aa7f269c496dc9dc97
Ceryni <matthew.fitzpatrick6012@gmail.com>
2020-03-11 04:34:18 +0000

    Generated logs

--- rottenpotatoes/spec/controllers/directors_controller_spec.rb
+++ rottenpotatoes/spec/controllers/directors_controller_spec.rb
@@ -2,11 +2,5 @@ require 'rails_helper'
 
 RSpec.describe DirectorsController, type: :controller do
 
-  describe "GET #movies" do
-    it "returns http success" do
-      get :movies
-      expect(response).to have_http_status(:success)
-    end
-  end
 
 end
